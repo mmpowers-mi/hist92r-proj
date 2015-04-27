e62d79e6392be3d495d2ce0d92ab87fa6f07f651
Marcus Powers <marcuspowers@college.harvard.edu>
2015-04-27 01:04:05 -0400

    first draft larger-scale testing

--- hist92r_script.py
+++ hist92r_script.py
@@ -1,41 +1,49 @@
 # Hist92r Parsing Script
 
+import os.path
+
 # main() definition--------------------------------------------------------
 def main():
-    # Read in a file to parse
-    fi = open("input/125.txt", "r")
-    contents = fi.read()
-    fi.close()
-    words = contents.split()
-    articles = []
-    article = []
-    alen = 0
-    valid = False
-
-    # Split into articles
-    for word in words:
-        if alen > 10 and word.isupper() and len(word) > 2 and valid:
-            alen = 0
-            articles.append(article)
+    # For every file in our folder
+    for num in range(1, 100):
+        # Read in a file to parse
+        instr = "input/" + str(num) + ".txt"
+        if os.path.isfile(instr):
+            fi = open(instr, "r")
+            contents = fi.read()
+            fi.close()
+            words = contents.split()
+            articles = []
             article = []
+            alen = 0
             valid = False
-        if word == "Overseas":
-            valid = True
-        article.append(word)
-        alen += 1
+
+            # Split into articles
+            for word in words:
+                if alen > 10 and word.isupper() and len(word) > 2:
+                    alen = 0
+                    if valid:
+                        articles.append(article)
+                    article = []
+                    valid = False
+                if word == "Overseas":
+                    valid = True
+                article.append(word)
+                alen += 1
+                
+            # Output resulting article(s)
+            ostr = "output/" + str(num) + ".txt"
+            fo = open(ostr, "w")
+            for a in articles:
+                for w in a:
+                    fo.write(w)
+                    fo.write(" ")
+                fo.write("\n\n")
+                print a
+                print
+                print
             
-    # Output resulting article(s)
-    fo = open("output/125.txt", "w")
-    for a in articles:
-        for w in a:
-            fo.write(w)
-            fo.write(" ")
-        fo.write("\n\n")
-        print a
-        print
-        print
-    
-    fo.close()
+            fo.close()
 
 # Run program-----------------------------------------------------------
 if __name__ == "__main__":
