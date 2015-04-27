4d36d583762182a4339eef7987d0317efbcab6e5
Marcus Powers <marcuspowers@college.harvard.edu>
2015-04-26 21:50:14 -0400

    First draft parsing articles

--- hist92r_script.py
+++ hist92r_script.py
@@ -3,26 +3,39 @@
 # main() definition--------------------------------------------------------
 def main():
     # Read in a file to parse
-    f = open("input/125.txt", "r")
-    contents = f.read()
+    fi = open("input/125.txt", "r")
+    contents = fi.read()
+    fi.close()
     words = contents.split()
     articles = []
     article = []
     alen = 0
+    valid = False
 
     # Split into articles
     for word in words:
-        if alen > 10 and word.isupper() and len(word) > 2:
+        if alen > 10 and word.isupper() and len(word) > 2 and valid:
             alen = 0
             articles.append(article)
             article = []
+            valid = False
+        if word == "Overseas":
+            valid = True
         article.append(word)
         alen += 1
             
-
-    print articles[9]
-
-    # Output resulting file
+    # Output resulting article(s)
+    fo = open("output/125.txt", "w")
+    for a in articles:
+        for w in a:
+            fo.write(w)
+            fo.write(" ")
+        fo.write("\n\n")
+        print a
+        print
+        print
+    
+    fo.close()
 
 # Run program-----------------------------------------------------------
 if __name__ == "__main__":
